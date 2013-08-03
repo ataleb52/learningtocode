c422e2785e9bfaf7ea7f231babc99717f19b287b
Ahmad Taleb <ataleb52@gmail.com>
2013-08-03 10:28:33 -0700

    Finished up a simple page testing out very basic jQuery functions along with Bootstrap

--- Epicodus Curric/JQuery Lesson/js/scripts2.js
+++ Epicodus Curric/JQuery Lesson/js/scripts2.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(".show_header").click(function() {
 		decision = prompt("Are you sure you want to begin experimenting on kitty cats")
 		if (decision === "yes") {
-			$("img").slideToggle();
+			$(".the_lab").fadeIn();
 			$(".show_header").fadeOut();
 			$(".noexperiment").hide();
 			$("#experiment").fadeIn();
@@ -11,4 +11,51 @@ $(document).ready(function() {
 			alert("Wise decision my friend")
 		}
 	});
+	$(".on").click(function() {
+		$("#peekaboocat").show();
+	})
+	
+	$(".off").click(function() {
+		$("#peekaboocat").hide();
+	})
+	
+	$(".toggle-btn").click(function() {
+		$("#togglecat").toggle();
+	})
+	
+	$(".fade-in-btn").click(function() {
+		$("#fadeincat").fadeIn();
+	})
+	
+	$(".fade-out-btn").click(function() {
+		$("#fadeincat").fadeOut();
+	})
+	
+	$(".slide-in-btn").click(function() {
+		$("#slidetoggle_cat").slideToggle();
+		$(".slide-in-btn").text('Slideout');
+	})
+	
+	$(".fade_toggle").click(function() {
+		$("#fadetoggle_cat").fadeToggle();
+	})
+	
+	$(".slideup-btn").click(function() {
+		$("#slideup_cat").slideUp();
+	})
+	
+	$(".slidedown-btn").click(function() {
+		$("#slideup_cat").slideDown();
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 })
