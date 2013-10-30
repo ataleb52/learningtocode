9addf9d32d316ca5aaaef9bf46cd48495ed53a39
Ahmad Taleb <ataleb52@gmail.com>
2013-10-30 13:49:42 -0700

    Starting up One Month Rails...for the 100th time

---
+++ onemonthrails/graftucson/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Graftucson::Application.config.secret_key_base = '3d24bc9a8598c876bc60030c8384bedfabdc90752ab01f526b47684d3006fa212e59373bc1acea20d29aa516167938fa62f35f9f219d42d2ace20c33af92019b'
---
+++ onemonthrails/graftucson/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Graftucson::Application.config.session_store :cookie_store, key: '_graftucson_session'
